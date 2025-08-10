998a703e5dd0729a78dd4878e65eebd55d668e6b
Corentin Giraud <corentin@giraud.dev>
2025-08-10 10:42:46 +0200

    Add backup-db scripts

---
+++ scripts/backup-db.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Directory to store backups
+BACKUP_DIR="/home/ubunutu/backups"
+DATE=$(date +\%Y-\%m-\%d)
+
+# Docker container name
+CONTAINER_NAME="take-a-seat-database"
+
+# Backup filename
+BACKUP_FILE="$BACKUP_DIR/$DATABASE_NAME-$DATE.dump"
+
+# Create backup inside Docker container using pg_dump
+docker exec -t $CONTAINER_NAME sh -c 'export PGUSER=$DATABASE_USERNAME_RO && export PGPASSWORD=$DATABASE_PASSWORD_RO && pg_dump -U $DATABASE_USERNAME_RO -F c $DATABASE_NAME' > $BACKUP_FILE
+
+# Delete backups older than 14 days
+find $BACKUP_DIR -type f -name "*.dump" -mtime +14 -exec rm {} \;
+
+echo "Backup completed for $DATABASE_NAME on $DATE"
