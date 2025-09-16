a7c5cfb2904e5fa9d681e4f1fe4c02ecab0704f8
Corentin Giraud <corentin@giraud.dev>
2025-09-16 18:06:25 +0200

    Edit backup-script

--- scripts/backup-db.sh
+++ scripts/backup-db.sh
@@ -3,13 +3,14 @@
 # Directory to store backups
 BACKUP_DIR="/home/ubuntu/backups"
 DATE=$(date +\%Y-\%m-\%d)
+TIME=$(date +\%H-\%M)
 
 # Docker container name
 CONTAINER_NAME="take-a-seat-database"
 DATABASE_NAME="take-a-seat"
 
 # Backup filename
-BACKUP_FILE="$BACKUP_DIR/$DATABASE_NAME-$DATE.dump"
+BACKUP_FILE="$BACKUP_DIR/$DATABASE_NAME-$DATE-$TIME.dump"
 
 # Create backup inside Docker container using pg_dump
 docker exec -t $CONTAINER_NAME sh -c 'export PGUSER=$DATABASE_USERNAME_RO && export PGPASSWORD=$DATABASE_PASSWORD_RO && pg_dump -U $DATABASE_USERNAME_RO -F c $DATABASE_NAME' > $BACKUP_FILE
@@ -17,4 +18,4 @@ docker exec -t $CONTAINER_NAME sh -c 'export PGUSER=$DATABASE_USERNAME_RO && exp
 # Delete backups older than 14 days
 find $BACKUP_DIR -type f -name "*.dump" -mtime +14 -exec rm {} \;
 
-echo "Backup completed for $DATABASE_NAME on $DATE"
+echo "Backup completed for $DATABASE_NAME on $DATE at $TIME"
