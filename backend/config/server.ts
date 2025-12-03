982f77ac3c221432890c9c7657a2ca634cf391d9
Corentin Giraud <corentin@giraud.dev>
2025-12-03 22:05:28 +0100

    feat: create users

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,8 +1,14 @@
+import cronTasks from "./cron-tasks";
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   url: env('API_PUBLIC_URL', 'http://localhost:1337'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  cron: {
+    enabled: true,
+    tasks: cronTasks,
   },
 });
